7f9696f1e9b2bc1434dedfb1cc27284e5e63b021
VanshSharmaS7C <vansh.sharma@seventhtriangle.com>
2023-07-26 22:49:37 +0530

    feat: :sparkles: Ex:5.7 => Add Hide and view button to show blog details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showFullDetails, setShowFullDetails] = useState(false)
+  const blogStyle = {
+    padding: '.5rem',
+    borderRadius: '10px',
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  const buttonTxt = showFullDetails ? 'hide' : 'view'
+  return (
+    <div style={blogStyle}>
+      <div>
+        <p>
+          {blog.title}{' '}
+          <button onClick={() => setShowFullDetails((prev) => !prev)}>
+            {buttonTxt}
+          </button>
+        </p>
+        {showFullDetails && (
+          <div>
+            <a href={blog.url}>{blog.url}</a>
+            <p>
+              likes: {blog.likes} <button>like</button>
+            </p>
+            <p>{blog.author}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
