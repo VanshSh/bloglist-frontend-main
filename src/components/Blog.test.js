5439508142345e5359d27963237c638ecdd3a006
VanshSharmaS7C <vansh.sharma@seventhtriangle.com>
2023-07-29 21:38:02 +0530

    test: :white_check_mark: Exercise: 5.15 => clicking the like button twice calls the event handler twice

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, screen } from '@testing-library/react'
+import blogService from '../services/blogs'
 import Blog from './Blog'
-
+jest.mock('../services/blogs')
 describe('Blog view tests', () => {
   let blog = {
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
+    user: {
+      username: 'micahelchan028',
+      name: 'Michael Chan',
+      id: '64bc14e31f8ad3fa0ac7a8c7',
+    },
+    id: '64bc163dd9dcf8d30229ffb8',
   }
 
   let mockUpdateBlog = jest.fn()
@@ -24,8 +31,7 @@ describe('Blog view tests', () => {
     )
     expect(view.container).toHaveTextContent('React patterns - Michael Chan')
   })
-
-  test('check the URL and number of likes', () => {
+  test('clicking the view button displays url and number of likes', () => {
     const view = render(
       <Blog
         blog={blog}
@@ -33,7 +39,24 @@ describe('Blog view tests', () => {
         deleteBlog={mockDeleteBlog}
       />
     )
+
+    const button = screen.getByText('view')
+    fireEvent.click(button)
+
     expect(view.container).toHaveTextContent('https://reactpatterns.com/')
     expect(view.container).toHaveTextContent('7')
+  })
+
+  test('clicking the like button twice calls the event handler twice', async () => {
+    render(<Blog blog={blog} handleLike={mockUpdateBlog} />)
+
+    // Find the like button
+    const likeButton = screen.getByText('like')
+
+    // Simulate two clicks on the like button
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(blogService.updateBlog).toHaveBeenCalledTimes(2)
   })
 })
